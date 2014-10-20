d167ea2868cc3c009f184fc51c2f5f0b90591bfd
charmi <charmijdedhia@gmail.com>
2014-10-20 15:33:48 +0530

    added back addPref

--- Candidate.java
+++ Candidate.java
@@ -41,8 +41,75 @@ public class Candidate {
 		this.index=i;
 	}
 	
-	public void addPreference(String prefList){
-		//currPref=0;			//complete
+	public void addPreference(String prefList){                    //function to create a preference list for the candidate
+		String pref;
+		if(prefList.indexOf('_')==-1) pref=prefList;               
+		else pref=prefList.substring(0,prefList.indexOf('_'));
+		int prev=-1,k=-1,last;
+		last=prefList.lastIndexOf('_');
+		do{
+			prev=k;
+			k=prefList.indexOf('_',k+1);
+			if(k!=-1) pref=prefList.substring(prev+1,k);
+			else pref=prefList.substring(last+1);
+			if(category==0){
+				preference.addElement(pref+"-0");           //if pref is B4110 then B4110-0 is added to preference list
+				preference.addElement(pref+"-1");       //0..7 represent categories as given in table of lab10 statement..top to down
+				preference.addElement(pref+"-4");
+				preference.addElement(pref+"-5");
+			}
+			else if(category==1){
+				preference.addElement(pref+"-0");
+				preference.addElement(pref+"-1");
+				preference.addElement(pref+"-4");
+				preference.addElement(pref+"-5");
+			}
+			else if(category==2){
+				preference.addElement(pref+"-0");
+				preference.addElement(pref+"-2");
+				preference.addElement(pref+"-1");
+				preference.addElement(pref+"-4");
+				preference.addElement(pref+"-5");
+				preference.addElement(pref+"-6");
+			}
+			else if(category==3){
+				preference.addElement(pref+"-0");
+				preference.addElement(pref+"-3");
+				preference.addElement(pref+"-1");
+				preference.addElement(pref+"-4");
+				preference.addElement(pref+"-5");
+				preference.addElement(pref+"-7");
+			}
+			else if(category==4){
+				preference.addElement(pref+"-0");
+				preference.addElement(pref+"-4");
+				preference.addElement(pref+"-1");
+				preference.addElement(pref+"-5");
+			}
+			else if(category==5){
+				preference.addElement(pref+"-0");
+				preference.addElement(pref+"-4");
+				preference.addElement(pref+"-1");
+				preference.addElement(pref+"-5");
+			}
+			else if(category==6){
+				preference.addElement(pref+"-0");
+				preference.addElement(pref+"-2");
+				preference.addElement(pref+"-4");
+				preference.addElement(pref+"-6");
+				preference.addElement(pref+"-1");
+				preference.addElement(pref+"-5");
+			}
+			else if(category==7){
+				preference.addElement(pref+"-0");
+				preference.addElement(pref+"-3");
+				preference.addElement(pref+"-4");
+				preference.addElement(pref+"-7");
+				preference.addElement(pref+"-1");
+				preference.addElement(pref+"-5");
+			}
+		} while(k!=-1);
+			
 	}
 		
 	
